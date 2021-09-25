98762e343395d606f2324b9c68ce10fd5fcbfa90
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-09-25 22:24:02 +0530

    Add files via upload

---
+++ Playing With Characters.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    printf("%c\n",ch);
+    scanf("\n");
+    char lone[100];
+    scanf("%s",&lone);  
+    printf("%s\n",lone);
+    scanf("\n"); 
+    char ltwo[100];
+    gets(ltwo);
+    printf("%s",ltwo);
+}
