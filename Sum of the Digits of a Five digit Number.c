12c54366a55981c95ab544130381228e0439703e
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-09-26 00:31:02 +0530

    Add files via upload

---
+++ Sum of the Digits of a Five digit Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum=0,temp;
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        temp=a%10;
+        a=a/10;
+        sum+=temp;
+    }
+    printf("%d",sum);
+}
